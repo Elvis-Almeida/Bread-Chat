87b4c93ac8bebbfe53e15c21050d8198f01b1023
Elvis-Almeida <lindete.r.almeida@gmail.com>
2022-05-07 21:45:06 -0300

    Adicionado back-end
    Co-authored-by: Victor101106 <Victor101106@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,16 @@
+const es6Renderer = require('express-es6-template-engine')
+const express = require('express');
+const app = express();
+
+app.engine('html', es6Renderer)
+app.set('views', __dirname + '\\Views')
+app.set('view engine', 'html')
+app.use(express.static(__dirname + '\\Public'))
+
+app.listen('5500', () => {
+    console.log('.\\Hello World!');
+});
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
